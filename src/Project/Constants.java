fc5cd279d117f5e5ad46027cb0490194ad196447
”Lamaret” <san4eknefedov@gmail.com>
2025-04-18 21:11:08 +0300

    Encrypt\Decrypt

--- src/Project/Constants.java
+++ src/Project/Constants.java
@@ -19,5 +19,6 @@ public final class Constants {
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                 'u', 'v', 'w', 'x', 'y', 'z')
+
     );
 };
